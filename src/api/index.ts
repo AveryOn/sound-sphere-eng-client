5bc2be2798c054dbb5db3ad93d9a8d5d8a817683
AveryOn <lnkonion2@gmail.com>
2025-05-04 01:53:51 +0500

    upd

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import chalk from "chalk";
 import moment from "moment";
 
 export async function ping() {
+    console.log('API_URL', import.meta.env.VITE_API_BASE_URL)
     const result = await axios.get(import.meta.env.VITE_API_BASE_URL + '/api/v2/ping')
     console.log(result);
     console.log(moment(Date.now()).format('HH:mm:ss'))
