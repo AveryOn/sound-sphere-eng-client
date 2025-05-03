5b30da55c7a82a7d9b4f59a4fbdaa05f74a9d8c3
AveryOn <lnkonion2@gmail.com>
2025-05-03 11:51:29 +0500

    upd v1.0.17

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ onMounted(() => {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! [PROD] v1.0.12" />
+      <HelloWorld msg="You did it! [PROD] v1.0.17" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
