e6faa27634b5840bd2373edb70ef5bc4f8aea29b
AveryOn <lnkonion2@gmail.com>
2025-05-03 20:34:06 +0500

    extends types env

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import chalk from "chalk";
 import moment from "moment";
 
 export async function ping() {
-    const result = await axios.get('http://localhost:3000/api/v2/ping')
+    const result = await axios.get(import.meta.env.VITE_API_BASE_URL + '/api/v2/ping')
     console.log(result);
     console.log(moment(Date.now()).format('HH:mm:ss'))
     console.log(chalk.white.bgBlack('lalal'))
