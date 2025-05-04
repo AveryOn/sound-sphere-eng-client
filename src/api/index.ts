5fbe9dfe9575bcb4515b4bd8f47c72855b675145
AveryOn <lnkonion2@gmail.com>
2025-05-04 09:58:58 +0500

    fix vite conifg

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import moment from "moment";
 
 export async function ping() {
     console.log('API_URL', import.meta.env.VITE_API_BASE_URL)
-    const result = await axios.get(import.meta.env.VITE_API_BASE_URL + '/api/v2/ping')
+    const result = await axios.get(import.meta.env.VITE_API_BASE_URL + '/v2/ping')
     console.log(result);
     console.log(moment(Date.now()).format('HH:mm:ss'))
     console.log(chalk.white.bgBlack('lalal'))
