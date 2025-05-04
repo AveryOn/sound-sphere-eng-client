1ac1efdcaa23065dbfe93974fcd8ecf6b73e0f71
AveryOn <lnkonion2@gmail.com>
2025-05-04 14:49:44 +0500

    init store

---
+++ src/stores/_index.ts
@@ -0,0 +1,9 @@
+import { ref, computed } from 'vue'
+
+export const useIndexStore = defineStore('_index', () => {
+
+
+    return {
+
+    }
+})
---
+++ src/stores/thrmes.ts
@@ -0,0 +1,4 @@
+
+export const useThemesStore = defineStore('themes', () => {
+  return { }
+})
